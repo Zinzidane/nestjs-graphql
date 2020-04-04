dc873bb95c79b70e01df83359634f576a5fddf42
ivan <Ivan Muliarchyk>
2020-04-04 22:56:05 +0300

    Add student resolver

---
+++ src/student/create-student.input.ts
@@ -0,0 +1,13 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
+
+@InputType()
+export class CreateStudentInput {
+    @MinLength(1)
+    @Field()
+    name: string;
+
+    @MinLength(1)
+    @Field()
+    surname: string
+}
